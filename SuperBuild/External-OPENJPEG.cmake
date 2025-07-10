249df70e4d88642469811f10af3dada847d6d21d
Taylor Hanayik <hanayik@gmail.com>
2025-07-10 12:35:07 +0100

    Update OpenJPEG version to v2.5.3 and match wasm build

--- SuperBuild/External-OPENJPEG.cmake
+++ SuperBuild/External-OPENJPEG.cmake
@@ -1,7 +1,7 @@
-set(OPENJPEG_TAG  openjpeg-2.5)
+set(OPENJPEG_TAG  v2.5.3)
 
 ExternalProject_Add(openjpeg
-    GIT_REPOSITORY "https://github.com/ningfei/openjpeg.git"
+    GIT_REPOSITORY "https://github.com/uclouvain/openjpeg.git"
     GIT_TAG "${OPENJPEG_TAG}"
     SOURCE_DIR openjpeg
     BINARY_DIR openjpeg-build
@@ -11,11 +11,18 @@ ExternalProject_Add(openjpeg
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
         -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
-	    # Compiler settings
         -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-        # Install directories
+        -DBUILD_SHARED_LIBS=OFF
+        # disable optional dependencies not needed for dcm2niix
+        -DBUILD_CODEC=OFF
+        -DBUILD_JPIP=OFF
+        -DBUILD_JPWL=OFF
+        -DBUILD_VIEWER=OFF
+        -DBUILD_JAVA=OFF
+        -DBUILD_MJ2=OFF
+        -DBUILD_THIRDPARTY=OFF
         -DCMAKE_INSTALL_PREFIX:PATH=${DEP_INSTALL_DIR}
 )
 
 include(GNUInstallDirs)
-set(OpenJPEG_DIR ${DEP_INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/${OPENJPEG_TAG})
+set(OpenJPEG_DIR ${DEP_INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/openjpeg-2.5)
