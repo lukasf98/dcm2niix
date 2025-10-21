9714e2f31a10aafeb40294dd8492f7d44bbd01fb
Taylor Hanayik <hanayik@gmail.com>
2025-10-21 16:08:46 +0100

    fix win vs unix builds

--- SuperBuild/External-OPENJPEG.cmake
+++ SuperBuild/External-OPENJPEG.cmake
@@ -1,5 +1,14 @@
 set(OPENJPEG_TAG  v2.5.3)
 
+# Set MSVC runtime flags only on Windows
+if(WIN32 AND MSVC)
+    set(OPENJPEG_C_FLAGS_RELEASE /MT)
+    set(OPENJPEG_C_FLAGS_DEBUG /MTd)
+else()
+    set(OPENJPEG_C_FLAGS_RELEASE "")
+    set(OPENJPEG_C_FLAGS_DEBUG "")
+endif()
+
 ExternalProject_Add(openjpeg
     GIT_REPOSITORY "https://github.com/uclouvain/openjpeg.git"
     GIT_TAG "${OPENJPEG_TAG}"
@@ -9,10 +18,10 @@ ExternalProject_Add(openjpeg
         -Wno-dev
         --no-warn-unused-cli
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        DCMAKE_POLICY_DEFAULT_CMP0091=NEW
+        -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
         -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded$<$<CONFIG:Debug>:Debug>
-        -DCMAKE_C_FLAGS_RELEASE=/MT
-        -DCMAKE_C_FLAGS_DEBUG=/MTd
+        -DCMAKE_C_FLAGS_RELEASE=${OPENJPEG_C_FLAGS_RELEASE}
+        -DCMAKE_C_FLAGS_DEBUG=${OPENJPEG_C_FLAGS_DEBUG}
         -DOPJ_USE_THREAD=OFF
         -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
         -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
@@ -28,6 +37,5 @@ ExternalProject_Add(openjpeg
         -DBUILD_THIRDPARTY=OFF
         -DCMAKE_INSTALL_PREFIX:PATH=${DEP_INSTALL_DIR}
 )
-
 include(GNUInstallDirs)
 set(OpenJPEG_DIR ${DEP_INSTALL_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/openjpeg-2.5)
